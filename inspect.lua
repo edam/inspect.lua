8e4d07c009f8b50ddeed2c0e4de2af41fe247a88
Tim Marston <tim.marston@lockwood-publishing.com>
2018-10-22 15:01:12 +0100

    added support to show/hide metatables

--- inspect.lua
+++ inspect.lua
@@ -265,7 +265,7 @@ function Inspector:putTable(t)
         count = count + 1
       end
 
-      if type(mt) == 'table' then
+      if type(mt) == 'table' and self.showmts then
         if count > 0 then self:puts(',') end
         self:tabify()
         self:puts('<metatable> = ')
@@ -287,7 +287,7 @@ function Inspector:putValue(v)
   local tv = type(v)
 
   if tv == 'string' then
-    self:puts(smartQuote(escape(v)))
+	 self:puts(smartQuote(escape(v)))
   elseif tv == 'number' or tv == 'boolean' or tv == 'nil' or
          tv == 'cdata' or tv == 'ctype' then
     self:puts(tostring(v))
@@ -307,6 +307,7 @@ function inspect.inspect(root, options)
   local newline = options.newline or '\n'
   local indent  = options.indent  or '  '
   local process = options.process
+  local showmts = options.showmts or true
 
   if process then
     root = processRecursive(process, root, {}, {})
@@ -320,6 +321,7 @@ function inspect.inspect(root, options)
     maxIds           = {},
     newline          = newline,
     indent           = indent,
+	showmts          = showmts,
     tableAppearances = countTableAppearances(root)
   }, Inspector_mt)
 
@@ -331,4 +333,3 @@ end
 setmetatable(inspect, { __call = function(_, ...) return inspect.inspect(...) end })
 
 return inspect
-
